fa21642e71a6aaa4d25cb7ad6deae7041f87632b
Tomas Horak <tom1horak@seznam.cz>
2019-07-21 20:24:57 +0200

    migrace na funkce sdilene knihovny

--- src/sensor_outside.ino
+++ src/sensor_outside.ino
@@ -1,5 +1,5 @@
-#include "shared.h"
-#include <SPI.h>
+#include "homeiot_shared.h"
+#include "nRF24L01.h"
 #include "RF24.h"
 
 
@@ -7,7 +7,7 @@
 RF24 nRF(9, 10);  // init pins CE, CS
 
 // OneWire
-const unsigned long SENSOR_VALUES_SEND_INTERVAL = 2UL * 5UL * 1000UL;			// other sensor notifications interval
+const unsigned long SENSOR_VALUES_SEND_INTERVAL = 60UL * 2UL * 1000UL;			// other sensor notifications interval
 unsigned long lastTemperatureSensorValuesSent = 0;								// time from last temperature sensor notification
 
 
@@ -22,29 +22,19 @@ void setup()
 	Serial.println(F("  RF24"));
 	nRF.begin();
 	nRF.stopListening();
-	nRF.setPALevel(RF24_PA_HIGH);
+	initRF(nRF);
 	nRF.openWritingPipe(RF_OUTSIDE_ADDRESS);
 
 	// settle time
 	delay(2000);
 
+
+	//Serial.println(F("Nastaveni nRF21: "));
+	//fdevopen(&serial_putc, 0);
+	//nRF.printDetails();
+	//Serial.println(F(""));
+
 	Serial.println(F("Nastartovano"));
-}
-
-void sendTemp(int id, float value)
-{
-	Serial.println("Odesilam teplotu cidla " + String(id) + ": " + String(value));
-
-	if (!nRF.write(&id, sizeof(id)))
-	{
-		Serial.println(F("Chyba pri odesilani 1!"));
-	}
-	delay(10);
-	if (!nRF.write(&value, sizeof(value)))
-	{
-		Serial.println(F("Chyba pri odesilani 2!"));
-	}
-	Serial.println(F("Odeslano"));
 }
 
 void loop()
@@ -53,7 +43,7 @@ void loop()
 
 	if ((time - lastTemperatureSensorValuesSent) > SENSOR_VALUES_SEND_INTERVAL)
 	{
-		sendTemp(RF_SENSOR_OUTSIDE_TEMPERATURE_ID, 15);
+		sendTemp(nRF, RF_SENSOR_OUTSIDE_TEMPERATURE_ID, 15.0);
 		lastTemperatureSensorValuesSent = time;
 	}
 }
